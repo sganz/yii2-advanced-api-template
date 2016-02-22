d4bc75dc0673db4b1538150872bcba2528c40cf1
Sandy <sganz@pacbell.net>
2016-02-22 09:29:20 -0800

    Removed _protected now protected. The `_` was ugly and didn't help sorting as the orig author thought...

--- _protected/common/translations/pt_BR/yii.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    '{attribute} cannot be blank.' => '{attribute} não pode estar em branco.',
-    'The verification code is incorrect.' => 'Código de verificação incorreto.',
-    'Home' => 'Início',
-    'You are not allowed to perform this action.' => 'Você não está autorizado a realizar esta ação.'
-];
